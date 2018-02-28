6494d5c8e49c270e24aab41f4275736eae181919
landyy <rmb2555@rit.edu>
2018-02-28 15:26:20 -0500

    um yeah
    yeah

--- win7.bat
+++ win7.bat
@@ -1,36 +1,52 @@
 @echo off
 
+:: Thank you Jamie!! <3
+
+:: Dc IP
 SET ip=192.168.58.128
 
+:: ESXI IP
+:: SET esxi=192.168.0.1
+
+:: Basic config things
 netsh advfirewall reset
-
+netsh advfirewall set allprofiles state on
 netsh advfirewall firewall delete rule name =all
+netsh advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
 
+:: DNS
 netsh advfirewall firewall add rule name="DNS" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=53 enable=yes profile=any
 netsh advfirewall firewall add rule name="DNS" dir=out remoteip=%ip% action=allow protocol=udp remoteport=53 enable=yes profile=any
+netsh advfirewall firewall add rule name="DNS" dir=in remoteip=%ip% action=allow protocol=tcp remoteport=53 enable=yes profile=any
+netsh advfirewall firewall add rule name="DNS" dir=in remoteip=%ip% action=allow protocol=udp remoteport=53 enable=yes profile=any
 
-netsh advfirewall firewall add rule name="LDAP" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=389 enable=yes profile=any
-netsh advfirewall firewall add rule name="LDAP UDP" dir=out remoteip=%ip% action=allow protocol=udp remoteport=389 enable=yes profile=any
-netsh advfirewall firewall add rule name="LDAP SSL" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=686 enable=yes profile=any
-netsh advfirewall firewall add rule name="LDAP GC" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=3268 enable=yes profile=any
-netsh advfirewall firewall add rule name="LDAP GC" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=3269 enable=yes profile=any
+:: AD/LDAP Client that is already joined to Domain
+netsh advfirewall firewall add rule name=AdClient dir=out protocol=tcp remoteport=88,389,445 remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=AdClinet dir=in protocol=tcp remoteport=88,389,445 remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=ADClinet dir=in protocol=tcp remoteport=88,389,445 remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=ADclinet dir=in protocol=udp remoteport=389 remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=ADclinet dir=out protocol=udp remoteport=389 remoteip=%ip% action=allow
 
-netsh advfirewall firewall add rule name="Kerberos" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=464 enable=yes profile=any
-netsh advfirewall firewall add rule name="Kerberos" dir=out remoteip=%ip% action=allow protocol=udp remoteport=464 enable=yes profile=any
-netsh advfirewall firewall add rule name="Kerberos" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=88 enable=yes profile=any
-netsh advfirewall firewall add rule name="Kerberos" dir=out remoteip=%ip% action=allow protocol=udp remoteport=88 enable=yes profile=any
+:: Join a Domain
+:: Pick one above or below depending on the situation :D
+netsh advfirewall firewall add rule name=join dir=in remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=join dir=out remoteip=%ip% action=allow
 
-netsh advfirewall firewall add rule name="Netbios" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=139 enable=yes profile=any
-netsh advfirewall firewall add rule name="Kerberos" dir=out remoteip=%ip% action=allow protocol=udp remoteport=138 enable=yes profile=any
-netsh advfirewall firewall add rule name="Kerberos" dir=out remoteip=%ip% action=allow protocol=udp remoteport=137 enable=yes profile=any
-
-netsh advfirewall firewall add rule name="RPC" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=135 enable=yes profile=any
-netsh advfirewall firewall add rule name="RPC" dir=out remoteip=%ip% action=allow protocol=udp remoteport=135 enable=yes profile=any
-netsh advfirewall firewall add rule name="RPC" dir=out remoteip=%ip% action=allow protocol=udp remoteport=5722 enable=yes profile=any
-netsh advfirewall firewall add rule name="RPC" dir=out remoteip=%ip% action=allow protocol=udp remoteport=5722 enable=yes profile=any
-
-netsh advfirewall firewall add rule name="RPC" dir=out remoteip=%ip% action=allow protocol=tcp remoteport=455 enable=yes profile=any
-
+:: Web browser rule, replace with firefox eventually 
 netsh advfirewall firewall add rule name="Allow Firefox" dir=in action=allow program="C:\Program Files\Mozilla Firefox\firefox.exe" enable=yes profile=any
+netsh advfirewall firewall add rule name="Allow Firefox" dir=out action=allow program="C:\Program Files\Mozilla Firefox\firefox.exe" enable=yes profile=any
+
+:: Syslog!
+netsh advfirewall firewall add rule name=syslog dir=out protocol=udp remoteport=514 action=allow
+
+:: Vsphere client
+:: If we go through browser, browser rule should be gucci
+netsh advfirewall firewall add rule name=vsphere dir=in protocol=tcp remoteport=443,902,903 remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=vsphere dir=in protocol=tcp remoteport=443,902,903 remoteip=%ip% action=allow
+
+:: RDP Client
+netsh advfirewall firewall add rule name=RDP dir=out protocol=tcp remoteport=3389 action=allow
+netsh advfirewall firewall add rule name=RDP dir=in protocol=tcp remoteport=3389 action=allow
 
 pause
+
