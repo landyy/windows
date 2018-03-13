59c6531b8bf6d035013e3a9f8c8637cb9d8422c0
landyy <rmb2555@rit.edu>
2018-03-13 10:19:20 -0400

    added some stuff

--- win7.bat
+++ win7.bat
@@ -22,19 +22,27 @@ netsh advfirewall firewall add rule name="DNS" dir=in action=allow protocol=tcp 
 netsh advfirewall firewall add rule name="DNS" dir=in action=allow protocol=udp remoteport=53 enable=yes profile=any
 
 :: AD/LDAP Client that is already joined to Domain
-netsh advfirewall firewall add rule name=AdClient dir=out protocol=tcp remoteport=88,389,445 remoteip=%ip% action=allow
-netsh advfirewall firewall add rule name=AdClinet dir=in protocol=tcp remoteport=88,389,445 remoteip=%ip% action=allow
-netsh advfirewall firewall add rule name=ADclinet dir=in protocol=udp remoteport=389 remoteip=%ip% action=allow
-netsh advfirewall firewall add rule name=ADclinet dir=out protocol=udp remoteport=389 remoteip=%ip% action=allow
+::LDAP is 389 and more
+netsh advfirewall firewall add rule name=AdClient dir=out protocol=tcp remoteport=88,445 remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=AdClinet dir=in protocol=tcp remoteport=88,445 remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=ADclinet dir=in protocol=udp remoteport=138,123 remoteip=%ip% action=allow
+netsh advfirewall firewall add rule name=ADclinet dir=out protocol=udp remoteport=138,123 remoteip=%ip% action=allow
 
-:: Join a Domain
-:: Pick one above or below depending on the situation :D
+::Allow for gpupdate to go through
+netsh advfirewall firewall add rule name=gp dir=out program="C:\Windows\System32\svchost.exe" remoteip=%ip% action=allow enable=yes profile=any
+netsh advfirewall firewall add rule name=gp dir=out program="C:\Windows\System32\lsass.exe" remoteip=%ip% action=allow enable=yes profile=any
+
+:: For joining a Domain
 netsh advfirewall firewall add rule name=join dir=in remoteip=%ip% action=allow
 netsh advfirewall firewall add rule name=join dir=out remoteip=%ip% action=allow
 
 :: Web browser rule, replace with firefox eventually 
 netsh advfirewall firewall add rule name="Allow Firefox" dir=in action=allow program="C:\Program Files\Mozilla Firefox\firefox.exe" enable=yes profile=any
 netsh advfirewall firewall add rule name="Allow Firefox" dir=out action=allow program="C:\Program Files\Mozilla Firefox\firefox.exe" enable=yes profile=any
+
+::DHCP Client
+netsh advfirewall firewall add rule name=dhcp dir=out protocol=udp remoteport=67 action=allow
+netsh advfirewall firewall add rule name=dhcp dir=in protocol=udp localport=68 action=allow
 
 :: Syslog!
 netsh advfirewall firewall add rule name=syslog dir=out protocol=udp remoteport=514 action=allow
